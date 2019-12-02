62dc8aebcf1a4f15eff53bbe396244a0bc3382ae
Kenny Choi <kkenc.gg@gmail.com>
2019-12-02 02:34:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function theBeatlesPlay(musicians, instruments ){
+  
+  var arr=[];
+  
+  for(var x = 0; x < musicians.length; x++){
+    arr[x] = musicians[x] + " plays " + instruments[x];
+  }
+  
+  return
+}
+function johnLennonFacts(){
+  var arr=[];
+  var counter = 0;
+while(facts.length <= 0){
+  arr[counter] = facts + "!!!"
+}
+return arr;
+}
+
+function iLoveTheBeatles(num){
+  var arr=[];
+  do{
+    arr.add("I love the Beatles!");
+    num ++;
+  }while(num < 15)
+}
